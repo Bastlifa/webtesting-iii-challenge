4bee5329d206d3b3a9349376fa1ca23edd6ac076
Eli Sacks <bastlifa@gmail.com>
2019-08-19 15:23:42 -0400

    upgraded dependencies, added some tests for display, dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react'
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  // 2. write this test
+    it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); 
+    expect(tree.toJSON()).toMatchSnapshot();
+    });
+});
+
+describe('<Display />', () =>
+{
+    it('defaults to unlocked and open', () =>
+    {
+        const { queryByText } = render(<Dashboard />)
+        expect(queryByText(/unlocked/i)).toBeTruthy()
+        expect(queryByText(/open/i)).toBeTruthy()
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render, fireEvent} from '@testing-library/react'
+import Display from './Display'
+
+describe('<Display />', () =>
+{
+    it('defaults to unlocked and open', () =>
+    {
+        const { queryByText } = render(<Display locked={true} closed={false} />)
+        expect(queryByText(/locked/i)).toBeTruthy()
+        expect(queryByText(/open/i)).toBeFalsy()
+    })
+})
